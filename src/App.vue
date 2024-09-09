d0645d67c9e86059c2a133c0c5a28ea9573b6920
elakapmain <notquadrilingual@gmail.com>
2024-09-09 16:00:41 -0500

    Website framework complete

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import {onMounted, ref} from 'vue'
+  
+  const divinationArray = ref([])
+  const discoveredArray = ref([])
+  const divined = ref('')
+
+  const diviner = () => {
+    let random = randomFunc()
+
+    discoveredArray.value[random] = divinationArray.value[random]
+
+    divined.value = divinationArray.value[random]
+  }
+
+  const loadDivinations = () => {
+    fetch('src/assets/divinations.txt').then(getDivinations).then(processData)
+  }
+
+  const getDivinations = (divinationArray) => {
+    return divinationArray.text()
+  }
+
+  const processData = (divinationsRaw) => {
+    // Split by new line, use regex to handle \r or \n
+    divinationArray.value = divinationsRaw.split(/\r?\n/);
+
+    for (let i = 0; i < divinationArray.value.length; i++) {
+      discoveredArray.value.push("Not yet discovered...");
+    }
+  }
+
+  const randomFunc = () => {
+    let num = Math.floor(Math.random() * divinationArray.value.length)
+
+    return num; 
+  }
+
+  onMounted( () => {
+    loadDivinations()
+  })
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="app">
+    <section class="heading">
+      <h1>Programming Project 2</h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <h2>Divine your fortune!</h2>
+    </section>
+
+    <section class="content">
+      <h3>What will you divine today?</h3>
+
+      <button id="divineFortune" type="button" v-on:click="diviner()">Divine!</button>
+      <p id="fortune">{{ divined }}</p>
+
+      <div>
+        <h2>Discovered Fortunes</h2>
+        <ul>
+          <li v-for="fortunes in discoveredArray">{{ fortunes }}</li>
+        </ul>
+      </div>
+    </section>
+  </main>
+</template>
